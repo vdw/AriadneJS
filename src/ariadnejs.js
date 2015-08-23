834aff517dc991681d65af812defdb22f447ab30
Dimitris Krestos <dkrestos@gmail.com>
2015-08-23 22:30:48 +0300

    Add delay option

--- src/ariadnejs.js
+++ src/ariadnejs.js
@@ -2,19 +2,12 @@
 
   this.AriadneJS = function() {
 
-    this.delay = null;
-
     var defaults = {
-
+      live:  false,
       delay: 600
-
     }
 
-    if (arguments[0] && typeof arguments[0] === "object") {
-
-      this.options = extendDefaults(defaults, arguments[0]);
-
-    }
+    this.options = extendDefaults(defaults, arguments[0]);
 
   }
 
@@ -22,7 +15,31 @@
   AriadneJS.prototype.init = function() {
 
     document.onmousemove = handleMouseMove;
+    var eventDoc, doc, body, pageX, pageY;
 
+    function handleMouseMove(event) {
+
+      event = event || window.event; // IE
+
+      // Calculate pageX/Y if missing and clientX/Y available
+      if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc      = eventDoc.documentElement;
+        body     = eventDoc.body;
+
+        event.pageX = event.clientX +
+          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+          (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+          (doc && doc.clientTop  || body && body.clientTop  || 0 );
+      }
+      pageX = event.pageX
+      pageY = event.pageY
+
+    }
+
+    setInterval(function() { console.log({cX: document.documentElement.clientWidth, cY: document.documentElement.clientHeight, X: pageX, Y: pageY}) }, this.options.delay);
   }
 
   // Private Methods
@@ -38,28 +55,6 @@
 
     return source;
 
-  }
-
-  function handleMouseMove(event) {
-    var eventDoc, doc, body, pageX, pageY;
-
-    event = event || window.event; // IE
-
-    // Calculate pageX/Y if missing and clientX/Y available
-    if (event.pageX == null && event.clientX != null) {
-      eventDoc = (event.target && event.target.ownerDocument) || document;
-      doc      = eventDoc.documentElement;
-      body     = eventDoc.body;
-
-      event.pageX = event.clientX +
-        (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-        (doc && doc.clientLeft || body && body.clientLeft || 0);
-      event.pageY = event.clientY +
-        (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-        (doc && doc.clientTop  || body && body.clientTop  || 0 );
-    }
-
-    console.log({cX: document.documentElement.clientWidth, cY: document.documentElement.clientHeight, X: event.pageX, Y: event.pageY})
   }
 
 }());
